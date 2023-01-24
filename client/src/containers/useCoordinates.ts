fecc953fb5d13d47ee54de021f53bc3d89e1db38
nerooc <tomasz_gajda@outlook.com>
2023-01-24 14:09:48 +0100

    Change map's initial center point

--- client/src/containers/useCoordinates.ts
+++ client/src/containers/useCoordinates.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { createContainer } from 'unstated-next';
 
 const useCoordinatesContainer = createContainer(() => {
-	const [coordinates, setCoordinates] = React.useState({ lat: 0, lng: 0 });
+	const [coordinates, setCoordinates] = React.useState({ lat: 51.482826095710024, lng: -0.005407333374023438 });
 
 	return {
 		coordinates,
